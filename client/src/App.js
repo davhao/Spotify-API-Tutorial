34c2cb0d5250dc357e33b582ddc7b8e980a8f943
hao-david <55423829+hao-david@users.noreply.github.com>
2020-05-09 02:09:00 -0400

    Added color, artist name, trying to add profile pic

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
     }
     this.state = {
       loggedIn: token ? true : false,
-      nowPlaying: { name: 'Not Checked', albumArt: '' }
+      nowPlaying: { name: 'Not Checked', artist: '', albumArt: '' },
+      profilePic: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-1/c32.32.405.405a/s320x320/521734_429079457155526_949696252_n.jpg?_nc_cat=102&_nc_sid=0c64ff&_nc_ohc=YiY9U6VQnOYAX9ruJ4U&_nc_ht=scontent-iad3-1.xx&oh=567a6641c2be180d48edceb3c9ebf4da&oe=5ED9F6BF'
     }
   }
   getHashParams() {
@@ -30,27 +31,44 @@ class App extends Component {
     return hashParams;
   }
 
-  getNowPlaying(){
+  getNowPlaying() {
     spotifyApi.getMyCurrentPlaybackState()
       .then((response) => {
         this.setState({
           nowPlaying: { 
-              name: response.item.name, 
+              name: response.item.name,
+              artist: response.item.artists[0].name, 
               albumArt: response.item.album.images[0].url
             }
         });
       })
   }
-  
+
+  // getAccountInfo() {
+  //   spotifyApi.getMe()
+  //   .then((response) => {
+  //     this.setState({
+  //       profilePic: response.images[0].url
+  //     })
+  //   })
+  // }
+
   render() {
     return (
       <div className="App">
-        <a href='http://localhost:8888' > Login to Spotify </a>
+        <a href='http://localhost:8888' > 
+          <button onClick={() => this.getAccountInfo()}>
+            Login With Spotify
+          </button> 
+        </a>
         <div>
-          Now Playing: { this.state.nowPlaying.name }
+          <img src={this.state.profilePic} style={{ height: 50 }} alt={""}/>
         </div>
         <div>
-          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} alt={""}/>
+          Now Playing: { this.state.nowPlaying.name } by { this.state.nowPlaying.artist }
+        </div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 300 }} alt={""}/>
         </div>
         <button onClick={() => this.getNowPlaying()}>
           Check Now Playing
